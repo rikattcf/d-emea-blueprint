604b9401196526b473099e32499c8fbee2e277f6
rikattcf <168676836+rikattcf@users.noreply.github.com>
2026-06-25 18:09:03 +0100

    Update BrandIcon.tsx

--- src/components/icons/BrandIcon.tsx
+++ src/components/icons/BrandIcon.tsx
@@ -6,7 +6,7 @@ export type BrandIconProps = {
 
 function BrandIcon(props: BrandIconProps) {
   return (
-   <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"  x="0px" y="0px" viewBox="0 0 423.9 62.8" className={props.className}
+   <svg xmlns="http://www.w3.org/2000/svg"  x="0px" y="0px" viewBox="0 0 423.9 62.8" className={props.className}
  >
  <style type="text/css">
   .st0{fill:#0DB02B;}
